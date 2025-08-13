879d6895b8489c30eb145e7233dc85d5b6edd9e8
Yuanqing Wang <wangyq@wangyq.net>
2025-08-13 12:50:14 +0800

    larger network

--- scripts/run.py
+++ scripts/run.py
@@ -6,7 +6,7 @@ wandb.login(
     key="58466296c2de2fdd61d262115503afdf302441b7",
 )
 from datetime import datetime
-name = "no-graph" + datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+name = "large-network" + datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 wandb.init(
     project="mojito",
     name=name,
