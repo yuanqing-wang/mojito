f9dc248630e81868390810efc54454ddb663e71c
Yuanqing Wang <wangyq@wangyq.net>
2025-10-22 00:46:01 -0400

    eval

--- scripts/eval.py
+++ scripts/eval.py
@@ -5,9 +5,28 @@ from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
 from datasets import Dataset
 from run import get_data, add
+import re
+from mojito.tokenizer import preprocess
+
+def tokenize(prompt, tokenizer):
+    result = tokenizer(prompt, padding="max_length", max_length=1024, return_tensors="pt")
+    result["labels"] = result["input_ids"].clone()
+    return result
+
+def get_data(point, tokenizer):
+    input_text = point['input']
+    prompt = f"[INST]{input_text}[/INST]"
+    prompt = re.sub(r'(<SMILES>[^<;]*);([^<]*</SMILES>)', r'\1</SMILES> <SMILES>\2', prompt)    
+    try:
+        prompt = preprocess(prompt)
+        prompt = prompt.replace("SMILES>", "MOJITO>")
+    except Exception as e:
+        print(f"Error processing prompt: {prompt}")
+    point["preprocessed_prompt"] = prompt
+    return tokenize(prompt, tokenizer)
 
 def run():
-    ckpt = "results/checkpoint-10000/"
+    ckpt = "results/"
     peft_cfg = PeftConfig.from_pretrained(ckpt)
     base = peft_cfg.base_model_name_or_path
     tokenizer = AutoTokenizer.from_pretrained(base)
